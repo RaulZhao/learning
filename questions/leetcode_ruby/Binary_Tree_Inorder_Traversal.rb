4142b6d4b0e953ccf649407370c6d49f5c695788
Yingbo(Raul) Zhao <Raul.Zhao@Spirent.com>
2022-08-21 16:39:37 -0700

    Create Binary_Tree_Inorder_Traversal.rb

---
+++ questions/leetcode_ruby/Binary_Tree_Inorder_Traversal.rb
@@ -0,0 +1,32 @@
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[]}
+
+def inorder_traversal(root)
+    @results = []
+    traverse(root)
+    @results
+end
+
+def traverse(node)
+    return if node.nil?
+    unless node.left.nil?
+        traversal(node.left)
+    end
+    unless node.nil?
+        @results << node.val
+    end
+    unless node.right.nil?
+        traversal(node.right)
+    end 
+end
